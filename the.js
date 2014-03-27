0ea02d0ec76aecb990c096e9545dfdde60e972fb
Evan Hahn <me@evanhahn.com>
2014-03-26 21:18:31 -0400

    i think we're ready to go

--- the.js
+++ the.js
@@ -1,26 +1,64 @@
 var _ = require('underscore');
 var crel = require('crel');
-var stringify = require('json-stringify-safe');
 
+var MAX_DEPTH = 3;
 var VARIABLES = [
-  'navigator'
+  'location',
+  'navigator',
+  'document'
 ];
 
-_(VARIABLES).each(function(variable) {
+function makeDL (variable, depth) {
 
-  var $header = crel('h2', variable + ':');
-  var $dl = crel('dl');
+  depth = depth || 0;
 
-  _.map(window[variable], function(value, key) {
+  if (depth > MAX_DEPTH)
+    return crel('div', '[value too deep]');
+
+  var result = crel('dl');
+
+  var keys = _(variable).keys();
+  _(keys).each(function (key) {
+
     if (key !== 'window') {
+
+      var value = variable[key];
+      if (value === variable)
+        return;
+
+      var val;
+      if (value === null) {
+        val = 'null';
+      } else if (_(value).isUndefined()) {
+        val = 'undefined';
+      } else if (_(value).isFunction()) {
+        val = value.toString();
+      } else if (!_(value.length).isUndefined() && !_(value).isString()) {
+        val = makeDL(value, depth + 1);
+      } else {
+        val = value.toString();
+      }
+
+      if (val === '') {
+        val = '[empty string]';
+      }
+
       var $dt = crel('dt', key);
-      var $dd = crel('dd', value.toString());
-      $dl.appendChild($dt);
-      $dl.appendChild($dd);
+      var $dd = crel('dd', val);
+      result.appendChild($dt);
+      result.appendChild($dd);
+
     }
+
   });
 
+  return result;
+
+}
+
+_(VARIABLES).each(function (variable) {
+  var $header = crel('h2', variable + ':');
+  var $dl = makeDL(window[variable]);
   document.body.appendChild($header);
   document.body.appendChild($dl);
-
 });
